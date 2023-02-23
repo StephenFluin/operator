751c216692f0b0d7262d7007113309ebf4e0e1c1
Stephen Fluin <stephen.fluin@gmail.com>
2023-02-23 14:45:04 -0600

    add title and ga primitives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
+// declare var ga: any;
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -8,7 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-operator';
-
+  constructor(router: Router, title: Title) {
+    
+    router.events.pipe(filter((e) => e.type === 'NavigationEnd'))
+    .subscribe((n: any) => {
+      title.getTitle();
+      window.scrollTo(0, 0);
+      // ga('send', 'pageview', n.urlAfterRedirects);
+      // gtag('config', 'G-EQV8B2CF0C', { page_path: n.urlAfterRedirects });
+  });
+  }
   
 }
 
